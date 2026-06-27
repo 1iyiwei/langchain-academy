6f17182353a9ad79970eeb4f764df450553a141d
Li-Yi Wei <git@liyiwei.org>
2026-06-27 15:11:50 +0800

    add grok api

--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,6 @@ for i in {1..5}; do
   cp module-$i/studio/.env.example module-$i/studio/.env
   echo "OPENAI_API_KEY=\"$OPENAI_API_KEY\"" > module-$i/studio/.env
   echo "GOOGLE_API_KEY=\"$GOOGLE_API_KEY\"" >> module-$i/studio/.env
+  echo "XAI_API_KEY=\"$XAI_API_KEY\"" >> module-$i/studio/.env
 done
 echo "TAVILY_API_KEY=\"$TAVILY_API_KEY\"" >> module-4/studio/.env
--- tests/google_xai_api.py
+++ tests/google_xai_api.py
@@ -1,12 +1,15 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_xai import ChatXAI
 from typing_extensions import TypedDict, Annotated
 from langgraph.graph.message import add_messages
+import random
 
 # 1. Initialize the free-tier Gemini model
 # os.environ["GOOGLE_API_KEY"] = "your-gemini-api-key"
 # Using gemini-2.5-flash for maximum free-tier quota limits
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+google_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+grok_llm = ChatXAI(model="grok-4")
 
 # 2. Define your Graph State
 class State(TypedDict):
@@ -14,6 +17,7 @@ class State(TypedDict):
 
 # 3. Define the LLM Node
 def chatbot_node(state: State):
+    llm = google_llm if "Google" in state["messages"][-1].content else grok_llm
     response = llm.invoke(state["messages"])
     return {"messages": [response]}
 
@@ -30,6 +34,9 @@ app = workflow.compile()
 # 5. Run the Graph
 if __name__ == "__main__":
     # Example input
-    user_message = "It is a rainy day today."
+    llm_to_use = "Google" if random.random() < 0.5 else "Grok"  # Randomly choose LLM for demo purposes
+
+    user_message = "Hi " + llm_to_use + " how is the weather today?"
+    print(user_message)
     response = app.invoke({"messages": [{"role": "user", "content": user_message}]})
     print(response["messages"][-1].content)  # Print final AI response
